b7dd6602f0d2babb6aa69952bff9d653d3441f7d
por <hello@timbormans.com>
2017-11-23 22:41:50 +0100

    Build named png images

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-const charToCode = char =>
-  char.split('').map(c => ('0000' + c.charCodeAt(0).toString(16)).slice(-4)).join('-');
+const fs = require('fs');
+const emoji = require('emojilib');
+const fontkit = require('fontkit');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 
-module.exports = (emoji) => require('./assets/' + charToCode(emoji));
+const collectionPath = '/System/Library/Fonts/Apple Color Emoji.ttc'; // >= OS X 10.12
+const fontPath = '/System/Library/Fonts/Apple Color Emoji.ttf'; // <= OS X 10.11
+
+let font;
+
+if (fs.existsSync(collectionPath)) {
+  [font] = fontkit.openSync(collectionPath).fonts;
+} else if (fs.existsSync(fontPath)) {
+  font = fontkit.openSync(fontPath);
+} else {
+  console.log('⚠️ Could not find the emoji font');
+  process.exit(1);
+}
+
+rimraf.sync(`${__dirname}/images`);
+mkdirp.sync(`${__dirname}/images`);
+
+const dictionary = emoji.ordered
+  .map(name => ({ name, value: emoji.lib[name].char }))
+  .filter(char => char.value)
+  .reduce((data, char) => Object.assign({}, data, { [char.value]: char.name }), {});
+
+for (let i = 0; i < font.numGlyphs; i += 1) {
+  const glyph = font.getGlyph(i);
+  const strings = font.stringsForGlyph(i);
+  const image = glyph.getImageForSize(160);
+
+  if (image && strings.length) {
+    const char = strings[0];
+    const name = dictionary[char];
+
+    fs.writeFileSync(`${__dirname}/images/${name}.png`, image.data);
+  }
+}
